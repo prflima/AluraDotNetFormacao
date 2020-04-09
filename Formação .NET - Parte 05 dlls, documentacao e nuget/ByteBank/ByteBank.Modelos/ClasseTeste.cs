b5108e09aa9c93d67884a938ca085b8ad64b37af
Paulo Lima <paulo.lima@expertsystem.com.br>
2020-04-09 12:41:10 -0300

    Documentando nossa biblioteca

---
+++ Formação .NET - Parte 05 dlls, documentacao e nuget/ByteBank/ByteBank.Modelos/ClasseTeste.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    class ClasseTeste
+    {
+        public ClasseTeste()
+        {
+            ModificadoresTeste teste = new ModificadoresTeste();
+            teste.MetodoPublico();
+        }
+    }
+
+    class TesteProtected : ModificadoresTeste
+    {
+        public TesteProtected()
+        {
+            MetodoProtected();
+        }
+    }
+    
+    public class ModificadoresTeste
+    {
+        public void MetodoPublico()
+        {
+            // Vísivel fora da classe "ModificadoresTeste"
+        }
+
+        private void MetodoPrivate()
+        {
+            // Vísivel apenas dentro da classe "ModificadoresTeste"
+        }
+
+        protected void MetodoProtected()
+        {
+            // Vísivel apenas na classe "ModificadoresTeste" e derivados(herancas)
+        }
+
+        internal void MetodoInternal()
+        {
+            // Vísivel apenas no Projeto ByteBank.Modelos
+        }
+    }
+}
