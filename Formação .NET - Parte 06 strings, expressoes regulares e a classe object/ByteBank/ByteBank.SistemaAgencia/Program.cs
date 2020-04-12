dd665aeeb34673a0fa2157b20b79dea06c74dbd6
Paulo Lima <paulo.lima@expertsystem.com.br>
2020-04-11 23:05:33 -0300

    Parte 06 - startswith, endwith, contains e regex

---
+++ Formação .NET - Parte 06 strings, expressoes regulares e a classe object/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,41 @@
+﻿using ByteBank.Modelos;
+using Humanizer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string urlParametros = "https://www.bytebank.com.br/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
+            //ExtratorDeValorURL extrator = new ExtratorDeValorURL(urlParametros);
+
+            //string valor = extrator.GetValor("moedaOrigem");
+            //Console.WriteLine("Valor de moedaOrigem: " + valor);
+
+            //string valorMoedaDestino = extrator.GetValor("moedaDestino");
+            //Console.WriteLine("Valor de moedaDestino: " + valorMoedaDestino);
+
+
+            //string padrao = "[0-9]{4}[-][0-9]{4}";
+            //string padrao = "[0-9]{4,5}[-]{0,1}[0-9]{4}";
+            //string padrao = "[0-9]{4,5}-{0,1}[0-9]{4}";
+            string padrao = "[0-9]{4,5}-?[0-9]{4}";
+            string textoTeste = "auosduoda adbau 4002-8922 asdnoadnd asinodo";
+
+            Match resultado = Regex.Match(textoTeste, padrao);
+
+
+
+            Console.WriteLine(resultado);
+            Console.ReadLine();
+        }        
+    }
+}
